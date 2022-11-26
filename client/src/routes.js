1a45ce57f1c3336b7c9438564449f39a5d814935
Anand Sahni <anandsahni57@gmail.com>
2022-11-26 12:10:26 +0530

    finished with expense tracker

---
+++ client/src/routes.js
@@ -0,0 +1,42 @@
+import App from './App';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Category from './pages/Category';
+import { createBrowserRouter } from 'react-router-dom' 
+import CheckAuth from './utils/CheckAuth';
+import Guest from './utils/Guest';
+
+
+
+export default createBrowserRouter([
+    {
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <CheckAuth>
+                        <Home />
+                    </CheckAuth>,
+        },
+        {
+          path: "/login",
+          element: <Guest>
+                        <Login />
+                    </Guest>
+        },
+        {
+          path: "/register",
+          element: <Guest>
+                        <Register />
+                    </Guest>
+        },
+        {
+          path: "/category",
+          element: <CheckAuth>
+                        <Category />
+                    </CheckAuth>
+        },
+      ],
+    },
+  ]);
